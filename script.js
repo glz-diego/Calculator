57f19ce729c655a262feee26db06b8a265cb1e5c
glz-diego <glzdiego12@gmail.com>
2019-04-02 18:12:56 -0500

    init message

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var calculator = new Object();
+var add = false;
+var sub = false;
+var div = false;
+var mul = false;
+var x = 0;
+var y = 0;
 function one(){
 	var textnode = document.createTextNode(1);
 	document.getElementById("output").appendChild(textnode);
@@ -39,53 +45,51 @@ function zero(){
 	var textnode = document.createTextNode(0);
 	document.getElementById("output").appendChild(textnode);
 }
-function clear(){
-	let output = document.querySelector("#output");
-	output.innerHTML = "";
-}
-function first(){
-	var first = document.querySelector("#output").value;
-	if (output != "") {
-		let output = document.querySelector("#output");
-		output.innerHTML = "";
-	}
-}
-function second(){
-	var second = document.querySelector("#output").value;
-	if (output != "") {
-		let output = document.querySelector("#output");
-		output.innerHTML = "";
-	}
-}
-var calculate = function() {
-			console.dir(this);
+
+
+// function x(){
+// 	var x = document.querySelector("#output").value;
+// 	if (output != "") {
+// 		let output = document.querySelector("#output");
+// 		output.innerHTML = "";
+// 	}
+// }
+// function y(){
+// 	var y = document.querySelector("#output").value;
+// 	if (output != "") {
+// 		let output = document.querySelector("#output");
+// 		output.innerHTML = "";
+// 	}
+// }
+// var calculate = function() {
+// 			console.dir(this);
 			
-			var x = document.querySelector(first);
-			var y = document.querySelector(second);
+// 			var x = document.querySelector(x);
+// 			var y = document.querySelector(y);
 			
-			var result;
+// 			var result;
 
-			if(this.id == "btnadd") {
-				result = calculator.add(x.value, y.value);
-				var textnode = document.createTextNode(result);
-				document.getElementById("output").appendChild(textnode);
-			}
+// 			if(this.id == "btnadd") {
+// 				result = calculator.add(x.value, y.value);
+// 				var textnode = document.createTextNode(result);
+// 				document.getElementById("output").appendChild(textnode);
+// 			}
 
-			else if (this.id == "btnsub") {
-				result = calculator.subtract(x.value, y.value); 
-				console.log(result);
-			}
+// 			else if (this.id == "btnsub") {
+// 				result = calculator.subtract(x.value, y.value); 
+// 				console.log(result);
+// 			}
 
-			else if (this.id == "btnmul") {
-				result = calculator.multiply(x.value, y.value); 
-				console.log(result);
-			}
+// 			else if (this.id == "btnmul") {
+// 				result = calculator.multiply(x.value, y.value); 
+// 				console.log(result);
+// 			}
 
-			else if (this.id == "btndiv") {
-				result = calculator.divide(x.value, y.value); 
-				console.log(result);
-			}
+// 			else if (this.id == "btndiv") {
+// 				result = calculator.divide(x.value, y.value); 
+// 				console.log(result);
+// 			}
 
-			var out = document.querySelector("#output");
-			output.value = result;
-}
+// 			var out = document.querySelector("#output");
+// 			output.value = result;
+// }
